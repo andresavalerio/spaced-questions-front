a1c3a759ee5e558f5559c511fa566e278d05124c
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2023-09-05 22:53:17 -0300

    fix: added classname at NoteEditor component

--- src/components/note-editor/NoteEditor.tsx
+++ src/components/note-editor/NoteEditor.tsx
@@ -9,7 +9,7 @@ function NoteEditor({ content, onContentChange }: NoteEditorProps) {
   return (
     <textarea
       value={content}
-      className={".note-editor-textarea"}
+      className={"note-editor-textarea"}
       onChange={(e) => onContentChange(e.target.value)}
     />
   );
