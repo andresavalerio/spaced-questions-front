75ed68841ebce7e009883db95cb2048bb7659cd9
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-09-18 08:03:13 -0300

    fix: header componentization

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,62 +1,19 @@
-import { useState, useRef, useEffect } from "react";
-import "./Header.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
-import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import UserImage from "./user-image/UserImage";
 
-function Header({ content, showUserImage = true }: { content: string; showUserImage?: boolean }) {    
-    const [isTooltipVisible, setTooltipVisible] = useState(false);
-    const tooltipRef = useRef<HTMLDivElement | null>(null);
-    const userImageRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-        // Função para detectar se um clique foi fora da tooltip ou do ícone do usuário
-        function handleClickOutside(event: MouseEvent) {
-            if (tooltipRef.current && !tooltipRef.current.contains(event.target as Node) && 
-                userImageRef.current && !userImageRef.current.contains(event.target as Node)) {
-                setTooltipVisible(false);
-            }
-        }
-
-        // Adiciona o event listener
-        document.addEventListener("mousedown", handleClickOutside);
-
-        // Limpa o event listener ao desmontar
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
-
-    return (
-        <div className="Header">
-            <div>
-                {content}
-            </div>
-
-            <div>
-                {showUserImage && (
-                    <>
-                        <div 
-                            className="Header-userImage" 
-                            ref={userImageRef} // Adiciona a ref ao ícone do usuário
-                            onClick={() => setTooltipVisible(!isTooltipVisible)}
-                        />
-                        {isTooltipVisible && (
-                            <div 
-                                className="Header-tooltip"
-                                ref={tooltipRef} // Adiciona a ref à tooltip
-                            >
-                                <Link to="/">
-                                <FontAwesomeIcon className="faArrowRightFromBracket" icon={faArrowRightFromBracket} />
-                                <span>Sair</span>
-                                </Link>
-                            </div>
-                        )}
-                    </>
-                )}
-            </div>
-        </div>
-    );
+function Header({
+  content,
+  showUserImage
+}: {
+  content: string;
+  showUserImage?: boolean;
+}) {
+  return (
+    <div className={styles.Header}>
+      <div>{content}</div>
+      <UserImage showUserImage={showUserImage}/>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/components/header/user-image/UserImage.tsx
@@ -0,0 +1,63 @@
+import { useState, useRef, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import styles from "./UserImage.module.css";
+
+const UserImage = ({ showUserImage = true }) => {
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+  const userImageRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // Fun��o para detectar se um clique foi fora da tooltip ou do �cone do usu�rio
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        tooltipRef.current &&
+        !tooltipRef.current.contains(event.target as Node) &&
+        userImageRef.current &&
+        !userImageRef.current.contains(event.target as Node)
+      ) {
+        setTooltipVisible(false);
+      }
+    }
+
+    // Adiciona o event listener
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Limpa o event listener ao desmontar
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div>
+      {showUserImage && (
+        <>
+          <div
+            className={styles["Header-userImage"]}
+            ref={userImageRef} // Adiciona a ref ao �cone do usu�rio
+            onClick={() => setTooltipVisible(!isTooltipVisible)}
+          />
+          {isTooltipVisible && (
+            <div
+              className={styles["Header-tooltip"]}
+              ref={tooltipRef} // Adiciona a ref � tooltip
+            >
+              <Link to="/">
+                <FontAwesomeIcon
+                  className="faArrowRightFromBracket"
+                  icon={faArrowRightFromBracket}
+                />
+                <span>Sair</span>
+              </Link>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default UserImage;
