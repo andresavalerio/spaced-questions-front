7300cb492758a07d840e5664f90ed5c4b4a71881
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-09-18 16:25:45 -0300

    feat: register component on subpage

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,25 +1,21 @@
-import { Link, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import Header from "../../components/header/Header";
+import { Route, Routes } from "react-router-dom";
+
 import { useLoginStore } from "../../stores/login/LoginStore";
 import styles from "./LoginPage.module.css";
 import LoginForms from "./LoginForms/LoginForms";
-import Register from "../register/Register";
 import ForgotPassword from "../forgot-password/ForgotPassword";
+import RegisterForms from "./RegisterForms/RegisterForms";
 
 const LoginPage = () => {
   const { setUser } = useLoginStore((state) => state);
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-
+  
   return (
     <div className={styles["login-container"]}>
       <div className={styles["logo-container"]}>Spaced Questions</div>
       <div className={styles["login-area-container"]}>
         <Routes>
           <Route index element={<LoginForms />}></Route>
-          <Route path="register" element = {<Register />}></Route>
+          <Route path="register" element = {<RegisterForms />}></Route>
           <Route path="forgot-password" element={<ForgotPassword/>}></Route>
         </Routes>
       </div>
