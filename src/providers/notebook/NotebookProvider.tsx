2a6a2f646b3c306395a7e0309e1a16d3f2a6cc2b
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-11-07 09:46:11 -0300

    fix: NotebooksReducers typs wrong name on Notebook Provider

--- src/providers/notebook/NotebookProvider.tsx
+++ src/providers/notebook/NotebookProvider.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { notebookReducer } from "./reducer/NotebookReducer";
-import { NotebookReducer, NotebookState } from "./reducer/reducerTypes";
+import { NotebookReducers, NotebookState } from "./reducer/reducerTypes";
 
 const initialState: NotebookState = { loading: false };
 
 export const NotebookContext = React.createContext<NotebookState>(initialState);
 
 export const NotebookDispatchContext =
-  React.createContext<React.Dispatch<NotebookReducer> | null>(null);
+  React.createContext<React.Dispatch<NotebookReducers> | null>(null);
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [notebookState, dispatch] = React.useReducer(
