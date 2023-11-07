552d709372db9b4ba481bcaf4d3460990b5222e4
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-11-07 09:42:37 -0300

    feat: notebook hooks get and remove notebook implementation

--- src/providers/notebook/hooks/NotebookHooks.spec.tsx
+++ src/providers/notebook/hooks/NotebookHooks.spec.tsx
@@ -4,6 +4,7 @@ import { UserProvider } from "../NotebookProvider";
 import { ReactNode } from "react";
 import { setupMockServer } from "helpers/tests";
 import { notebookHandlers } from "../api/NotebookMockServer";
+import { act } from "react-dom/test-utils";
 
 const wrapper = ({ children }: { children: ReactNode }) => (
   <UserProvider>{children}</UserProvider>
@@ -14,4 +15,41 @@ const renderNotebookHooks = () =>
 
 describe("NotebookHooks", () => {
   setupMockServer(notebookHandlers);
+
+  it("NotebookHooks should be rendered", () => {
+    const { result } = renderNotebookHooks();
+
+    expect(result.current.state).toBeDefined();
+    expect(result.current.actions).toBeDefined();
+  });
+
+  describe("Get notebook", () => {
+    it("notebook hooks should get the notebook by notebook's name and owner", async () => {
+      const { result } = renderNotebookHooks();
+
+      await act(async () => {
+        await result.current.actions.getNotebook("pedro", "Caderno de Física");
+      });
+
+      expect(result.current.state).toHaveProperty("loading", false);
+      expect(result.current.state).toHaveProperty("data");
+      expect(result.current.state.data![0]).toHaveProperty("name");
+    });
+  });
+
+  describe("remove notebook", () => {
+    it("should remove one notebook", async () => {
+      const { result } = renderNotebookHooks();
+
+      await act(async () => {
+        await result.current.actions.removeNotebook(
+          "pedro",
+          "Caderno de Física"
+        );
+      });
+
+      expect(result.current.state).toHaveProperty("loading", false);
+      expect(result.current.state).toHaveProperty("data", undefined);
+    });
+  });
 });
--- src/providers/notebook/hooks/NotebookHooks.ts
+++ src/providers/notebook/hooks/NotebookHooks.ts
@@ -1,12 +1,14 @@
 import React from "react";
 import { NotebookContext, NotebookDispatchContext } from "../NotebookProvider";
 import {
-
   NotebookDispatch,
   NotebookReducerTypes,
   NotebookState,
-
 } from "../reducer/reducerTypes";
+import {
+  requestDeleteNotebook,
+  requestNotebookByName,
+} from "../api/NotebookAPI";
 
 const { LOADED, LOADING, DELETE, ERROR } = NotebookReducerTypes;
 
@@ -19,50 +21,59 @@ export const useNotebookProvider = () => {
   const state = useNotebookContext();
 
   if (!dispatch)
-    throw new Error("Must be Defined in a User Provider Component");
+    throw new Error("Must be Defined in a Notebook Provider Component");
 
-  return { state, actions: {} };
+  return {
+    state,
+    actions: {
+      getNotebook: getNotebookAction(state, dispatch),
+      removeNotebook: removeNotebookAction(state, dispatch),
+    },
+  };
 };
 
-export const createLoadingNotebookAction =
-  (state: NotebookState, dispatch: NotebookDispatch) =>
-{
-    //Ver se já ta carregando
-    if(state.loading){
-
+export const createLoadingNotebookAction = (
+  state: NotebookState,
+  dispatch: NotebookDispatch
+) => {
+  //Ver se já ta carregando
+  if (state.loading) {
     //Se não estiver
     dispatch({ type: LOADING });
+  }
+  //Se estiver
+  //Null, ignora a request
+};
+
+export const getNotebookAction =
+  (state: NotebookState, dispatch: NotebookDispatch) =>
+  async (owner: string, notebook: string): Promise<void> => {
+    try {
+      dispatch({ type: LOADING });
+
+      const response = await requestNotebookByName(owner, notebook);
+
+      dispatch({
+        type: LOADED,
+        payload: response.notebooks,
+      });
+    } catch (error) {
+      dispatch({ type: ERROR });
+      console.error(error);
     }
-    //Se estiver
-    //Null, ignora a request
-}
+  };
 
+export const removeNotebookAction =
+  (state: NotebookState, dispatch: NotebookDispatch) =>
+  async (owner: string, notebook: string): Promise<void> => {
+    try {
+      dispatch({ type: LOADING });
 
+      await requestDeleteNotebook(owner, notebook);
 
-
-
-// export const createLoginUserAction =
-//   (state: UserState, dispatch: UserDispatch) =>
-//   async (login: string, password: string): Promise<void> => {
-//     try {
-//       if (isUserLogged(state)) {
-//         throw new UserAlreadyLoggedError();
-//       }
-
-//       dispatch({ type: LOADING });
-
-//       const response = await requestUserLogin(login, password);
-
-//       if (!response) throw new Error();
-
-//       localStorage.setItem("token", response.token);
-
-//       dispatch({
-//         type: LOGIN,
-//         payload: response.user,
-//       });
-//     } catch (error) {
-//       dispatch({ type: ERROR });
-//       throw error;
-//     }
-//   };
+      dispatch({ type: DELETE, payload: undefined });
+    } catch (error) {
+      dispatch({ type: ERROR });
+      console.log(error);
+    }
+  };
