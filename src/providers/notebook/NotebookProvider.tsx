0944a7173bb141ab9299a651fe932ccd39ec04c2
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-11-07 09:41:41 -0300

    fix: types update on notebook provider

--- src/providers/notebook/NotebookProvider.tsx
+++ src/providers/notebook/NotebookProvider.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { notebookReducer } from "./reducer/NotebookReducer";
-import { NotebookReducers, NotebookState } from "./types";
+import { NotebookReducer, NotebookState } from "./reducer/reducerTypes";
 
 const initialState: NotebookState = { loading: false };
 
 export const NotebookContext = React.createContext<NotebookState>(initialState);
 
 export const NotebookDispatchContext =
-  React.createContext<React.Dispatch<NotebookReducers> | null>(null);
+  React.createContext<React.Dispatch<NotebookReducer> | null>(null);
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [notebookState, dispatch] = React.useReducer(
