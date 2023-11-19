9595f7e523b9f36205aae6c0125de1f3bc57e845
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2023-11-19 16:38:00 -0300

    chore: notebook new modifications button css and html

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,23 +1,30 @@
 import { MouseEventHandler } from "react";
-import './Tab.css';
+import styles from "./Tab.module.css";
 
 function Tab({
   label,
   isActive,
   onClick,
-  }: {
+}: {
   label: string;
   isActive: boolean;
   onClick: MouseEventHandler;
 }) {
   return (
     <div
+      className={`${styles["tab-container"]} ${
+        isActive ? styles["active"] : styles["inactive"]
+      }`}
       onClick={onClick}
-      className={`tab-container ${isActive ? 'active' : 'inactive'}`}
     >
-      <span className="tab-label">
-        {label}
-      </span>
+      {isActive && (
+        <div className={styles["alt-container"]}>
+          <span className={`${styles["dot"]} ${styles["change-name"]}`}><img src="/chave-de-rosca.svg"></img></span>
+          <span className={`${styles["dot"]} ${styles["delete"]}`}><img src="/lixeira.svg"></img></span>
+        </div>
+      )}
+
+      <span className="tab-label">{label}</span>
     </div>
   );
 }
